59f64a79d3a631f00f500169ef2b10befb625d95
Peymer <ipigp@yandex.ru>
2024-12-22 01:40:56 +0300

    modified:   index.mjs

--- index.mjs
+++ index.mjs
@@ -6,13 +6,9 @@ import Fv from '@fastify/view'
 import Furl from '@fastify/formbody'
 
 let Q=[
-// let Q1=
 "Вопрос первый очень важный. Вопрос первый очень важный. Вопрос первый очень важный. Вопрос первый очень важный. Вопрос первый очень важный. Вопрос первый очень важный. Вопрос первый очень важный. ",
-// let Q2=
 "Вопрос второй, на который ответить не может никто. Вопрос второй, на который ответить не может никто. Вопрос второй, на который ответить не может никто. Вопрос второй, на который ответить не может никто. Вопрос второй, на который ответить не может никто. ",
-// let Q3=
 "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-// let Q=[Q1,Q2,Q3];
 ];
 
 const __dirname=import.meta.dirname;
