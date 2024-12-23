230821520bbf7f08e038e4c905264642e8d6aa4f
Peymer <ipigp@yandex.ru>
2024-12-23 20:33:34 +0300

    new file:   answers 	modified:   index.mjs 	modified:   node_modules/.package-lock.json 	modified:   package-lock.json 	modified:   package.json 	modified:   questions.ejs 	new file:   questions1.ejs

--- index.mjs
+++ index.mjs
@@ -6,11 +6,13 @@ import Fv from '@fastify/view';
 import Furl from '@fastify/formbody';
 import lr from 'line-reader';
 import {qlist} from './loadtickets.mjs';
+import Datastore from 'nedb';
 
 let Q=qlist("questions.txt");
-// lr.eachLine('questions.txt', (line) => {
-//   Q.push(line);
-// });
+
+//СУБД NeDB
+var db = new Datastore({filename : 'answers'});
+db.loadDatabase();
 
 let students=[];
 lr.eachLine("students.txt", (line)=>{
@@ -48,29 +50,73 @@ server.get("/", async (q,a)=>{
   return a.sendFile("login.html");
 });
 
+server.get("/q", async (q,a) => {
+  let name="Иванопуло";
+  let ticket=1;
+    //neDb
+    // db.insert({name : name, ticket: ticket}, (err)=>{if (err) console.log(`db error ${err}`)});
 
-server.post("/q", async (q,a)=>{
-  let {name, ticket} = q.body;
-  if (students.findIndex((el)=> el == name) >= 0)
-  // let Q=[Q1, Q2]//, Q3];
-  //let A=[A1, A2];
-    return a.view('questions.ejs', {name: name, ticket: ticket, Q: Q[ticket]});
-  else
-    return a.redirect("/");
+  return a.view('questions.ejs', {name: name, ticket: ticket, Q: Q[ticket-1].Questions});
+});
+
+// server.post("/q", async (q,a)=>{
+//   let {name, ticket} = q.body;
+//   if (students.findIndex((el)=> el == name) >= 0){
+
+//   //neDb
+//     return a.view('questions.ejs', {name: name, ticket: ticket, Q: Q[ticket]});
+//   }
+//   else{
+//     return a.redirect("/");
+//   }
+// });
+server.get("/a", async (q,a)=>{
+  let name="Иванопуло";
+  let ticket=1;
+  let msg;
+  let aaaa=db.find({name:name});
+  console.log(aaaa);
+
+  db.find({name: name}, function (err, docs) {
+    if (err) 
+      msg=`db error find ${err}`;
+    else{
+      msg=docs[0];
+      console.dir(docs[0]);
+      console.dir(msg);
+      return a.send(msg);
+    }
+  });
 });
 
 server.post("/ejs", async (q,a)=>{
   let {name, ticket, A} = q.body;
-  // let Q=[Q1, Q2];//, Q3];
-  // let A=[A1, A2, A3];
-  // if (question === undefined)
-  //   question=0;
-  return a.view('answer.ejs', {name: name, ticket: ticket, Q: Q[ticket], A:A})
+ 
+  //neDb
+  db.insert({name : name, ticket: Number(ticket), A:A}, (err)=>{if (err) console.log(`db error ${err}`)});
+
+//  console.log('before update');
+//  db.find({name: name, ticket: Number(ticket)}, function (err, docs) {
+//     if (err) 
+//       console.log(`db error find ${err}`);
+//     console.log(docs);
+//   });
+
+//   db.update({name: name, ticket: Number(ticket)}, {name:name,ticket:Number(ticket),A:A}, {}, (err, numberOfUpdated)=>{if (err) console.log(`db error update ${err}`); console.log(numberOfUpdated);});
+  
+//   console.log('after update');
+//   db.find({name: name, ticket: Number(ticket)}, function (err, docs) {
+//     if (err) 
+//       console.log(`db error find ${err}`);
+//     console.log(docs);
+//   });
+
+  return a.view('answer.ejs', {name: name, ticket: ticket, Q: Q[ticket].Questions, A:A})
 });
 
-server.setErrorHandler(async (error, q,a)=>{
-  return a.sendFile(path.join(__dirname,'404.html'));
-});
+// server.setErrorHandler(async (error, q,a)=>{
+//   return a.sendFile(path.join(__dirname,'404.html'));
+// });
 
 server.listen({ port: 3000 }, (err, address) => {
     if (err) throw err
