05f814f3d50414aa7efdcc230d5387cd0a2ebfc5
Igor Peymer <ipigp@yandex.ru>
2024-12-26 22:26:36 +0300

    modified:   answers.db 	modified:   index.mjs 	modified:   login.html

--- index.mjs
+++ index.mjs
@@ -8,8 +8,6 @@ import lr from 'line-reader';
 import {qlist} from './loadtickets.mjs';
 import Datastore from 'nedb-promises';
 
-// let Q=qlist("questions.txt");
-
 //СУБД NeDB
 var db = Datastore.create({filename : 'answers.db', autoload: true});
 var students = Datastore.create({filename : 'students.db', autoload: true});
@@ -17,11 +15,7 @@ var questions = Datastore.create({filename : 'questions.db', autoload: true});
 // db.loadDatabase();
 // NeDB
 
-// чтение студентов из файла
-// let students=[];
-// lr.eachLine("students.txt", (line)=>{
-//   students.push(line.trim());
-// });
+let bilets=[];
 
 var group='3ПС5';
 const __dirname=import.meta.dirname;
@@ -64,27 +58,30 @@ server.get("/", async (q,a)=>{
 //   return a.view('questions.ejs', {name: name, ticket: ticket, Q: Q[ticket-1].Questions});
 // });
 
+function newTicket(){
+  let bilet;
+  do
+  {
+    bilet = Math.floor(Math.random()*30)+1;
+  }
+  while (bilets.includes(bilet));
+  bilets.push(bilet);
+  return bilet;
+}
+
 server.post("/q", async (q,a)=>{
   let {name, ticket} = q.body;
+
   // проверка на студента
   let stud = await students.findOne({group: group, name: name});
   if (stud){
+    ticket=newTicket();
     let QQ=await questions.findOne({group: group, t:Number(ticket)});
     let Q=QQ.questions;
     return a.view('questions.ejs', {name: stud.name, surname: stud.surname, ticket: ticket, Q: Q});
   }else{
     return a.redirect("/");
   }
-});
-
-server.get("/a", async (q,a)=>{
-  // let name="Иванопуло";
-  // let ticket=1;
-
-  let aaaa=await db.find({});
-  
-  aaaa.forEach(el => { console.log(el)});
-  return a.send({aaaa});
 });
 
 server.post("/ejs", async (q,a)=>{
